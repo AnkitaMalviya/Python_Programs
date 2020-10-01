a81d68ca3f6159e8ac29a82d01afb69946128a20
AnkitaMalviya <72163210+AnkitaMalviya@users.noreply.github.com>
2020-10-01 12:10:12 +0530

    Update student_exam.py

--- if_else_codes-master/student_exam.py
+++ if_else_codes-master/student_exam.py
@@ -8,17 +8,17 @@
 #Modify the above question to allow student to sit if he/she has medical cause. Ask user if he/she has medical cause or not ( 'Y' or 'N' ) and print accordingly.
 student=raw_input("enter your choice Y and N=")
 if student=="Y":
-  print"you are able for exam"
-  print"no of classes held"
+  print("you are able to give exam")
+  print("no of classes held")
   held=input()
   print"no of classes attendence"
   attendence=input()
   total=100*attendence/held
   print"attendence",total
   if total>=75:
-    print"you allowed for exam"
+    print("you are allowed for exam")
   else:
-    print"you not allowed for exam"
+    print("you are not allowed for exam")
 else:
-  print"you not allowed for seat exam"
+  print("you are not allowed to seat for exam")
 
